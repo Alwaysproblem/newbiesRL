83e2936998e02f4390c70456726249d36f2157c7
Alwaysproblem <reganyang0415@gmail.com>
2022-10-27 12:40:03 +0800

    fomatter

--- try.py
+++ try.py
@@ -1,13 +1,15 @@
 # %%
-import numpy as np
-from collections import deque
-import torch 
+import numpy as np  # noqa: F401
+from collections import deque  # noqa: F401
+import torch
 import torch.nn as nn
+
 # %%
 # a = [1,2,3,4,5,6,7]
 # a = [1,1,2,2,3,4,5,6,7]
 # %%
 # np.random.choice( a , size = (7), replace = False, p = [0.5,  0.5])
+
 
 # %%
 class Q(nn.Module):
@@ -19,9 +21,9 @@ class Q(nn.Module):
 
     self.hidden_layers = [
         (nn.Linear(in_size, out_size), nn.GELU())
-        for in_size, out_size in zip((state_dim,) +
+        for in_size, out_size in zip((state_dim, ) +
                                      self.hidden_size, self.hidden_size +
-                                     (action_space,))
+                                     (action_space, ))
     ]
 
   def forward(self, state):
@@ -30,10 +32,11 @@ class Q(nn.Module):
       x = a(l(x))
     return x
 
+
 # %%
 q = Q(4, 2)
 # %%
-a = q(torch.Tensor([1.,2.,3.,4.]))
+a = q(torch.Tensor([1., 2., 3., 4.]))
 # %%
 a.argmax()
 # %%
