e88564982ce102fc1bc0f413ff27527fd17e0dd8
Alwaysprblem <reganyang0415@gmail.com>
2023-03-31 14:16:10 +0800

    change loop

--- AWR/main.py
+++ AWR/main.py
@@ -1,6 +1,7 @@
 """main executable file for A2C"""
 import os
 import logging
+from itertools import repeat
 import gym
 import torch
 import numpy as np
@@ -21,7 +22,12 @@ EPSILON_DECAY_STEPS = 100
 
 
 def main(
-    n_episodes=2000, max_t=2000, eps_start=1.0, eps_end=0.01, eps_decay=0.996
+    n_episodes=2000,
+    max_t=200,
+    eps_start=1.0,
+    eps_end=0.01,
+    eps_decay=0.996,
+    score_term_rules=lambda s: False
 ):
   # pylint: disable=line-too-long
   """Deep Q-Learning
@@ -43,16 +49,11 @@ def main(
 
   env = TrainMonitor(env, tensorboard_dir="./logs", tensorboard_write_all=True)
 
-  gamma = 0.995
-  lr_actor = 0.002
-  lr_critic = 0.002
-  batch_size = 64
-  beta = 0
-
   gamma = 0.99
   lr_actor = 0.001
   lr_critic = 0.001
   batch_size = 64
+  beta = 0
 
   td_lambda = 1.0
   awr_beta = 1.0
@@ -80,7 +81,7 @@ def main(
     state, _ = env.reset()
     score = 0
     traj = Trajectory()
-    for _ in range(max_t):
+    for _, _ in enumerate(repeat(0, max_t)):
       action = agent.take_action(state=state)
       next_state, reward, done, _, _ = env.step(action)
       traj.enqueue(Experience(state, action, reward, next_state, done))
@@ -88,7 +89,7 @@ def main(
       state = next_state
       score += reward
 
-      if done:
+      if done or score_term_rules(score):
         break
 
       scores_window.append(score)  ## save the most recent score
