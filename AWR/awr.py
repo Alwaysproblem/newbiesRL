86e1156f7ddead666f2063397d2b4c52f9a6db9b
Arno <31947147+Alwaysproblem@users.noreply.github.com>
2023-04-08 19:33:34 +0800

    Added AWR (#14)
    * added AWR
    
    * add n workers

---
+++ AWR/awr.py
@@ -0,0 +1,241 @@
+"""DQN implementation with pytorch."""
+import numpy as np
+import torch
+from torch import nn
+from torch.distributions import Categorical
+from torch.nn import functional as F
+
+from util.agent import Agent
+from util.buffer import ReplayBuffer, Trajectory
+
+
+def standardize(v):
+  """Method to standardize a rank-1 np array."""
+  assert len(v) > 1, "Cannot standardize vector of size 1"
+  v_std = (v - v.mean()) / (v.std() + 1e-08)
+  return v_std
+
+
+class Actor(nn.Module):
+  """ Actor (Policy) Model."""
+
+  def __init__(self, state_dim, action_space, seed=0, fc1_unit=64, fc2_unit=64):
+    """
+        Initialize parameters and build model.
+        Params
+        =======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_unit (int): Number of nodes in first hidden layer
+            fc2_unit (int): Number of nodes in second hidden layer
+        """
+    super().__init__()  ## calls __init__ method of nn.Module class
+    self.seed = torch.manual_seed(seed)
+    self.fc1 = nn.Linear(state_dim, fc1_unit)
+    self.fc2 = nn.Linear(fc1_unit, fc2_unit)
+    self.fc_policy = nn.Linear(fc2_unit, action_space)
+
+  def forward(self, x):
+    """
+        Build a network that maps state -> action values.
+        """
+    x = F.relu(self.fc1(x))
+    x = F.relu(self.fc2(x))
+    pi = F.softmax(self.fc_policy(x), dim=1)
+    return pi
+
+
+class Critic(nn.Module):
+  """ Critic (Policy) Model."""
+
+  def __init__(
+      self, state_dim, action_space=1, seed=0, fc1_unit=64, fc2_unit=64
+  ):
+    """
+        Initialize parameters and build model.
+        Params
+        =======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_unit (int): Number of nodes in first hidden layer
+            fc2_unit (int): Number of nodes in second hidden layer
+        """
+    super().__init__()  ## calls __init__ method of nn.Module class
+    self.seed = torch.manual_seed(seed)
+    self.fc1 = nn.Linear(state_dim, fc1_unit)
+    self.fc2 = nn.Linear(fc1_unit, fc2_unit)
+    self.fc3 = nn.Linear(fc2_unit, action_space)
+
+  def forward(self, x):
+    """
+        Build a network that maps state -> action values.
+        """
+    x = F.relu(self.fc1(x))
+    x = F.relu(self.fc2(x))
+    return self.fc3(x)
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class AWRAgent(Agent):
+  """Interacts with and learns form environment."""
+
+  def __init__(
+      self,
+      state_dims,
+      action_space,
+      gamma=0.99,
+      lr_actor=0.001,
+      lr_critic=0.001,
+      batch_size=64,
+      epsilon=0.01,
+      mem_size=None,
+      forget_experience=True,
+      td_lambda=1.0,
+      beta=0,
+      awr_beta=0.5,
+      awr_min_weight=0.1,
+      awr_max_weight=0.1,
+      seed=0
+  ):
+
+    self.state_dims = state_dims
+    self.action_space = action_space
+    self.gamma = gamma
+    self.batch_size = batch_size
+    self.epsilon = epsilon
+    self.seed = np.random.seed(seed)
+    self.td_lambda = td_lambda
+    self.lr_actor = lr_actor
+    self.lr_critic = lr_critic
+    self.beta = beta
+    self.awr_beta = awr_beta
+    self.awr_min_weight = awr_min_weight
+    self.awr_max_weight = awr_max_weight
+
+    #Q- Network
+    self.actor = Actor(state_dims, action_space).to(device)
+    self.critic = Critic(state_dims).to(device)
+
+    self.actor_optimizer = torch.optim.AdamW(
+        self.actor.parameters(), lr=self.lr_actor
+    )
+    self.critic_optimizer = torch.optim.AdamW(
+        self.critic.parameters(), lr=self.lr_critic
+    )
+
+    # Replay memory
+    self.memory = ReplayBuffer(max_size=mem_size)
+
+    self.forget_experience = forget_experience
+
+    self.val_loss = nn.MSELoss()
+    self.policy_loss = nn.MSELoss()
+
+  def learn(self, iteration: int = 10):
+    """Update value parameters using given batch of experience tuples."""
+    polcy_loss, val_loss = np.nan, np.nan
+    if len(self.memory) < self.batch_size:
+      return polcy_loss, val_loss
+
+    polcy_loss = []
+    val_loss = []
+    for _ in range(iteration):
+      trajectory = self.memory.sample_from()[0]
+      polcy_loss_, val_loss_ = self._learn(trajectory)
+      polcy_loss.append(polcy_loss_.cpu().data.numpy())
+      val_loss.append(val_loss_.cpu().data.numpy())
+
+    return np.array(polcy_loss).mean(), np.array(val_loss).mean()
+
+  def _learn(self, trajectory: Trajectory):
+    states = torch.from_numpy(np.vstack([e.state for e in trajectory])
+                              ).float().to(device)
+    actions = torch.from_numpy(np.vstack([e.action for e in trajectory])
+                               ).long().to(device)
+    rewards = torch.from_numpy(np.vstack([e.reward for e in trajectory])
+                               ).float().to(device)
+    next_states = torch.from_numpy(
+        np.vstack([e.next_state for e in trajectory])
+    ).float().to(device)
+    _ = torch.from_numpy(np.vstack([e.done
+                                    for e in trajectory])).float().to(device)
+
+    mcre = self.monte_carlo_rewards_estimates(rewards, states, next_states)
+    mcre = standardize(mcre)
+    val_loss = self.val_loss(mcre.detach(), self.critic.forward(states))
+    _, action_dist = self.action(state=states, mode="train")
+
+    advs = mcre - self.critic.forward(states).detach()
+    advs = standardize(advs)
+    adv_weight = torch.clip(
+        torch.exp(1 / self.awr_beta * advs), self.awr_min_weight,
+        self.awr_max_weight
+    )
+
+    policy_loss = -torch.mean(
+        adv_weight.detach() * action_dist.log_prob(actions.T).T -
+        self.beta * action_dist.entropy()
+    )
+
+    self.actor_optimizer.zero_grad()
+    self.critic_optimizer.zero_grad()
+    policy_loss.backward()
+    val_loss.backward()
+    self.actor_optimizer.step()
+    self.critic_optimizer.step()
+    return policy_loss, val_loss
+
+  def monte_carlo_rewards_estimates(self, rewards, states, next_states):
+    # computes td-lambda return of path
+    T = len(rewards)  # pylint: disable=invalid-name
+
+    v_preds = self.critic.forward(states).detach()
+    next_v_pred = self.critic.forward(next_states[-1]).detach()
+    val_t = torch.concat((v_preds, next_v_pred.unsqueeze(0)), dim=0)
+
+    assert len(val_t) == T + 1
+
+    return_t = torch.zeros(T)
+    last_val = rewards[-1] + self.gamma * val_t[-1]
+    return_t[-1] = last_val
+
+    for i in reversed(range(0, T - 1)):
+      curr_r = rewards[i]
+      next_ret = return_t[i + 1]
+      curr_val = curr_r + self.gamma * ((1.0 - self.td_lambda) * val_t[i + 1] +
+                                        self.td_lambda * next_ret)
+      return_t[i] = curr_val
+
+    return return_t.unsqueeze(1).to(device)
+
+  def action(self, state, mode="eval"):
+    if mode == "train":
+      self.actor.train()
+    else:
+      self.actor.eval()
+
+    pi = self.actor.forward(state)
+    dist = Categorical(pi)
+    action = dist.sample()
+    return action.cpu().data.numpy(), dist
+
+  def take_action(self, state, _=0):
+    """Returns action for given state as per current policy
+        Params
+        =======
+            state (array_like): current state
+            epsilon (float): epsilon, for epsilon-greedy action selection
+        """
+    state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+
+    with torch.no_grad():
+      action_values, *_ = self.action(state)
+
+    return action_values.item()
+
+  def remember(self, scenario: Trajectory):
+    self.memory.enqueue(scenario)
---
+++ AWR/main.py
@@ -0,0 +1,125 @@
+"""main executable file for A2C"""
+import os
+import logging
+from itertools import repeat
+import gym
+import torch
+import numpy as np
+from util import generate_gif
+from util.wrappers import TrainMonitor
+from util.buffer import Experience, Trajectory
+from collections import deque
+# pylint: disable=invalid-name
+from AWR.awr import AWRAgent as AWR_torch
+
+Agent = AWR_torch
+logging.basicConfig(level=logging.INFO)
+
+torch.manual_seed(0)
+np.random.seed(0)
+
+EPSILON_DECAY_STEPS = 100
+
+
+def main(
+    n_episodes=2000,
+    max_t=200,
+    eps_start=1.0,
+    eps_end=0.01,
+    eps_decay=0.996,
+    score_term_rules=lambda s: False
+):
+  # pylint: disable=line-too-long
+  """Deep Q-Learning
+
+    Params
+    ======
+        n_episodes (int): maximum number of training epsiodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): mutiplicative factor (per episode) for decreasing epsilon
+
+    """
+  scores = []  # list containing score from each episode
+  scores_window = deque(maxlen=100)  # last 100 scores
+  eps = eps_start
+  env = gym.make("LunarLander-v2", render_mode="rgb_array")
+  # env = gym.make("CartPole-v1", render_mode="rgb_array")
+
+  env = TrainMonitor(env, tensorboard_dir="./logs", tensorboard_write_all=True)
+
+  gamma = 0.99
+  lr_actor = 0.001
+  lr_critic = 0.001
+  batch_size = 64
+  beta = 0
+
+  td_lambda = 1.0
+  awr_beta = 1.0
+  awr_min_weight = 0
+  awr_max_weight = 20
+  learn_iteration = 100
+  num_workers = 10
+
+  agent = Agent(
+      state_dims=env.observation_space.shape[0],
+      action_space=env.action_space.n,
+      lr_actor=lr_actor,
+      lr_critic=lr_critic,
+      gamma=gamma,
+      batch_size=batch_size,
+      forget_experience=False,
+      beta=beta,
+      td_lambda=td_lambda,
+      awr_beta=awr_beta,
+      awr_min_weight=awr_min_weight,
+      awr_max_weight=awr_max_weight,
+  )
+  dump_gif_dir = f"images/{agent.__class__.__name__}/{agent.__class__.__name__}_{{}}.gif"
+
+  for i_episode in range(1, n_episodes + 1):
+    for _ in range(num_workers):
+      state, _ = env.reset()
+      score = 0
+      traj = Trajectory()
+      for _, _ in enumerate(repeat(0, max_t)):
+        action = agent.take_action(state=state)
+        next_state, reward, done, _, _ = env.step(action)
+        traj.enqueue(Experience(state, action, reward, next_state, done))
+
+        state = next_state
+        score += reward
+
+        if done or score_term_rules(score):
+          break
+
+        scores_window.append(score)  ## save the most recent score
+        scores.append(score)  ## sae the most recent score
+        eps = max(eps * eps_decay, eps_end)  ## decrease the epsilon
+        print(" " * os.get_terminal_size().columns, end="\r")
+        print(
+            f"\rEpisode {i_episode}\tAverage Score {np.mean(scores_window):.2f}",
+            end="\r"
+        )
+
+      agent.remember(traj)
+    agent.learn(learn_iteration)
+
+    if i_episode and i_episode % 100 == 0:
+      print(" " * os.get_terminal_size().columns, end="\r")
+      print(
+          f"\rEpisode {i_episode}\tAverage Score {np.mean(scores_window):.2f}"
+      )
+      generate_gif(
+          env,
+          filepath=dump_gif_dir.format(i_episode),
+          policy=lambda s: agent.take_action(s, 0),
+          max_episode_steps=max_t
+      )
+
+  return scores
+
+
+if __name__ == "__main__":
+  main()
