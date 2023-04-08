f18b3781440f711059d1e86b4e6dde8d04f15027
Alwaysprblem <reganyang0415@gmail.com>
2023-04-08 19:27:11 +0800

    add n workers

--- AWR/main.py
+++ AWR/main.py
@@ -60,6 +60,7 @@ def main(
   awr_min_weight = 0
   awr_max_weight = 20
   learn_iteration = 100
+  num_workers = 10
 
   agent = Agent(
       state_dims=env.observation_space.shape[0],
@@ -78,30 +79,31 @@ def main(
   dump_gif_dir = f"images/{agent.__class__.__name__}/{agent.__class__.__name__}_{{}}.gif"
 
   for i_episode in range(1, n_episodes + 1):
-    state, _ = env.reset()
-    score = 0
-    traj = Trajectory()
-    for _, _ in enumerate(repeat(0, max_t)):
-      action = agent.take_action(state=state)
-      next_state, reward, done, _, _ = env.step(action)
-      traj.enqueue(Experience(state, action, reward, next_state, done))
+    for _ in range(num_workers):
+      state, _ = env.reset()
+      score = 0
+      traj = Trajectory()
+      for _, _ in enumerate(repeat(0, max_t)):
+        action = agent.take_action(state=state)
+        next_state, reward, done, _, _ = env.step(action)
+        traj.enqueue(Experience(state, action, reward, next_state, done))
 
-      state = next_state
-      score += reward
+        state = next_state
+        score += reward
 
-      if done or score_term_rules(score):
-        break
+        if done or score_term_rules(score):
+          break
 
-      scores_window.append(score)  ## save the most recent score
-      scores.append(score)  ## sae the most recent score
-      eps = max(eps * eps_decay, eps_end)  ## decrease the epsilon
-      print(" " * os.get_terminal_size().columns, end="\r")
-      print(
-          f"\rEpisode {i_episode}\tAverage Score {np.mean(scores_window):.2f}",
-          end="\r"
-      )
+        scores_window.append(score)  ## save the most recent score
+        scores.append(score)  ## sae the most recent score
+        eps = max(eps * eps_decay, eps_end)  ## decrease the epsilon
+        print(" " * os.get_terminal_size().columns, end="\r")
+        print(
+            f"\rEpisode {i_episode}\tAverage Score {np.mean(scores_window):.2f}",
+            end="\r"
+        )
 
-    agent.remember(traj)
+      agent.remember(traj)
     agent.learn(learn_iteration)
 
     if i_episode and i_episode % 100 == 0:
