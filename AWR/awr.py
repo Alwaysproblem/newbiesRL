a3615d2fdb339f6f8e85bd35ee66cf2a251a1f3d
Alwaysprblem <reganyang0415@gmail.com>
2023-04-20 19:46:09 +0800

    refactor awr

--- AWR/awr.py
+++ AWR/awr.py
@@ -8,18 +8,38 @@ from torch.nn import functional as F
 from util.agent import Agent
 from util.buffer import ReplayBuffer, Trajectory
 
+NORMEPS = 1e-8
+
 
 def standardize(v):
   """Method to standardize a rank-1 np array."""
   assert len(v) > 1, "Cannot standardize vector of size 1"
-  v_std = (v - v.mean()) / (v.std() + 1e-08)
+  v_std = (v - v.mean()) / (v.std() + NORMEPS)
   return v_std
+
+
+def scale_up_values(v, mean=0, std=1, norm_factor=1):
+  return v / norm_factor * std + mean
+
+
+def scale_down_values(v, mean=0, std=1, norm_factor=1):
+  return norm_factor * (v - mean) / (std + NORMEPS)
 
 
 class Actor(nn.Module):
   """ Actor (Policy) Model."""
 
-  def __init__(self, state_dim, action_space, seed=0, fc1_unit=64, fc2_unit=64):
+  def __init__(
+      self,
+      state_dim,
+      action_space,
+      seed=0,
+      fc1_unit=256,
+      fc2_unit=256,
+      init_weight_gain=np.sqrt(2),
+      init_policy_weight_gain=0.01,
+      init_bias=0
+  ):
     """
         Initialize parameters and build model.
         Params
@@ -36,13 +56,22 @@ class Actor(nn.Module):
     self.fc2 = nn.Linear(fc1_unit, fc2_unit)
     self.fc_policy = nn.Linear(fc2_unit, action_space)
 
+    nn.init.orthogonal_(self.fc1.weight, gain=init_weight_gain)
+    nn.init.orthogonal_(self.fc2.weight, gain=init_weight_gain)
+    nn.init.uniform_(
+        self.fc_policy.weight, -init_policy_weight_gain, init_policy_weight_gain
+    )
+
+    nn.init.constant_(self.fc1.bias, init_bias)
+    nn.init.constant_(self.fc2.bias, init_bias)
+    nn.init.constant_(self.fc_policy.bias, init_bias)
+
   def forward(self, x):
     """
         Build a network that maps state -> action values.
         """
     x = F.relu(self.fc1(x))
     x = F.relu(self.fc2(x))
-    # x /= temperature  # 对原始得分进行缩放
     pi = F.softmax(self.fc_policy(x), dim=1)
     return pi
 
@@ -51,7 +80,15 @@ class Critic(nn.Module):
   """ Critic (Policy) Model."""
 
   def __init__(
-      self, state_dim, action_space=1, seed=0, fc1_unit=64, fc2_unit=64
+      self,
+      state_dim,
+      action_space=1,
+      seed=0,
+      fc1_unit=256,
+      fc2_unit=256,
+      init_weight_gain=np.sqrt(2),
+      init_value_weight_gain=1,
+      init_bias=0
   ):
     """
         Initialize parameters and build model.
@@ -68,6 +105,14 @@ class Critic(nn.Module):
     self.fc1 = nn.Linear(state_dim, fc1_unit)
     self.fc2 = nn.Linear(fc1_unit, fc2_unit)
     self.fc3 = nn.Linear(fc2_unit, action_space)
+
+    nn.init.xavier_uniform_(self.fc1.weight, gain=init_weight_gain)
+    nn.init.xavier_uniform_(self.fc2.weight, gain=init_weight_gain)
+    nn.init.xavier_uniform_(self.fc3.weight, gain=init_value_weight_gain)
+
+    nn.init.constant_(self.fc1.bias, init_bias)
+    nn.init.constant_(self.fc2.bias, init_bias)
+    nn.init.constant_(self.fc3.bias, init_bias)
 
   def forward(self, x):
     """
@@ -99,7 +144,13 @@ class AWRAgent(Agent):
       beta=0,
       awr_beta=0.5,
       awr_min_weight=0.1,
-      awr_max_weight=0.1,
+      awr_max_weight=100,
+      grad_clip=0.5,
+      norm_factor=1,
+      value_network_scale=True,
+      critic_train_step=10,
+      actor_train_step=100,
+      l2_loss_weight=0.01,
       seed=0
   ):
 
@@ -117,15 +168,30 @@ class AWRAgent(Agent):
     self.awr_min_weight = awr_min_weight
     self.awr_max_weight = awr_max_weight
 
+    self.grad_clip = grad_clip
+    self.norm_factor = norm_factor
+    self.value_network_scale = 1 / (
+        1 - self.gamma
+    ) if value_network_scale else 1.0
+
+    self.global_cnt = 0
+    self.global_mean = 0
+    self.global_std = 1
+
+    self.l2_loss_weight = l2_loss_weight
+
+    self.critic_train_step = critic_train_step
+    self.actor_train_step = actor_train_step
+
     #Q- Network
     self.actor = Actor(state_dims, action_space).to(device)
     self.critic = Critic(state_dims).to(device)
 
     self.actor_optimizer = torch.optim.Adam(
-        self.actor.parameters(), lr=self.lr_actor
+        self.actor.parameters(), lr=self.lr_actor, eps=1e-5
     )
     self.critic_optimizer = torch.optim.Adam(
-        self.critic.parameters(), lr=self.lr_critic
+        self.critic.parameters(), lr=self.lr_critic, eps=1e-5
     )
 
     # Replay memory
@@ -136,67 +202,84 @@ class AWRAgent(Agent):
     self.val_loss = nn.MSELoss()
     self.policy_loss = nn.MSELoss()
 
-  def learn(self, iteration: int = 10):
+  def learn(self, iteration: int = 10, replace=True):
     """Update value parameters using given batch of experience tuples."""
     polcy_loss, val_loss = np.nan, np.nan
-    if len(self.memory) < self.batch_size:
+    if len(self.memory) < iteration * self.batch_size:
       return polcy_loss, val_loss
 
     polcy_loss = []
     val_loss = []
-    for _ in range(iteration):
-      trajectory = self.memory.sample_from()[0]
-      polcy_loss_, val_loss_ = self._learn(trajectory)
-      polcy_loss.append(polcy_loss_.cpu().data.numpy())
+    trajectories = self.memory.sample_from(
+        num_samples=iteration, replace=replace
+    )
+    if not trajectories:
+      return np.nan, np.nan
+
+    for trajectory in trajectories:
+      val_loss_ = self._update_critic(trajectory)
       val_loss.append(val_loss_.cpu().data.numpy())
+
+    for trajectory in trajectories:
+      polcy_loss_ = self._update_actor(trajectory)
+      polcy_loss.append(polcy_loss_.cpu().data.numpy())
 
     return np.array(polcy_loss).mean(), np.array(val_loss).mean()
 
-  def _learn(self, trajectory: Trajectory):
+  def _update_critic(self, trajectory: Trajectory):
+    states = torch.from_numpy(np.vstack([e.state for e in trajectory])
+                              ).float().to(device)
+    rewards = torch.from_numpy(np.vstack([e.reward for e in trajectory])
+                               ).float().to(device)
+
+    mcre = self.td_lambda_estimate(rewards, states)
+    # mcre = standardize(mcre)
+    n_mcre = scale_down_values(mcre, std=self.value_network_scale)
+    val_loss = 0.5 * self.val_loss(n_mcre.detach(), self.critic.forward(states))
+    self.critic_optimizer.zero_grad()
+    val_loss.backward()
+    self.critic_optimizer.step()
+    return val_loss
+
+  def _update_actor(self, trajectory: Trajectory):
     states = torch.from_numpy(np.vstack([e.state for e in trajectory])
                               ).float().to(device)
     actions = torch.from_numpy(np.vstack([e.action for e in trajectory])
                                ).long().to(device)
     rewards = torch.from_numpy(np.vstack([e.reward for e in trajectory])
                                ).float().to(device)
-    next_states = torch.from_numpy(
-        np.vstack([e.next_state for e in trajectory])
-    ).float().to(device)
-    _ = torch.from_numpy(np.vstack([e.done
-                                    for e in trajectory])).float().to(device)
-
-    mcre = self.monte_carlo_rewards_estimates(rewards, states, next_states)
-    mcre = standardize(mcre)
-    val_loss = self.val_loss(mcre.detach(), self.critic.forward(states))
-    _, action_dist = self.action(state=states, mode="train")
-
-    advs = mcre - self.critic.forward(states).detach()
+    mcre = self.td_lambda_estimate(rewards, states)
+    advs = mcre - scale_up_values(
+        self.critic.forward(states), std=self.value_network_scale
+    ).detach()
     advs = standardize(advs)
     adv_weight = torch.clip(
         torch.exp(1 / self.awr_beta * advs), self.awr_min_weight,
         self.awr_max_weight
     )
+    _, action_dist = self.action(state=states, mode="train")
+    l2_loss = torch.sum(torch.square(action_dist.logits), dim=-1)
 
     policy_loss = -torch.mean(
-        adv_weight.detach() * action_dist.log_prob(actions.T).T -
-        self.beta * action_dist.entropy()
-    )
+        adv_weight.detach() * action_dist.log_prob(actions.T).T
+    ) + self.beta * action_dist.entropy().mean(
+    ) + self.l2_loss_weight * 0.5 * l2_loss.mean()
 
     self.actor_optimizer.zero_grad()
-    self.critic_optimizer.zero_grad()
     policy_loss.backward()
-    val_loss.backward()
+    nn.utils.clip_grad_norm_(self.actor.parameters(), self.grad_clip)
     self.actor_optimizer.step()
-    self.critic_optimizer.step()
-    return policy_loss, val_loss
 
-  def monte_carlo_rewards_estimates(self, rewards, states, next_states):
+    return policy_loss
+
+  def td_lambda_estimate(self, rewards, states):
     # computes td-lambda return of path
     T = len(rewards)  # pylint: disable=invalid-name
 
-    v_preds = self.critic.forward(states).detach()
-    next_v_pred = self.critic.forward(next_states[-1]).detach()
-    val_t = torch.concat((v_preds, next_v_pred.unsqueeze(0)), dim=0)
+    v_preds = scale_up_values(
+        self.critic.forward(states), std=self.value_network_scale
+    ).detach()
+    val_t = torch.concat((v_preds, torch.Tensor([[0]]).to(device)), dim=0)
 
     assert len(val_t) == T + 1
 
